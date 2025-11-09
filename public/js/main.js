eacc5fe9241422c8156bd3795365849d6f8279b0
Michael Schwartz <msschwartz@outlook.com>
2025-11-09 13:35:54 -0500

    delay

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,20 @@
-SpatialNavigation.init();
-SpatialNavigation.add({
-  selector: ".control",
-});
-SpatialNavigation.makeFocusable();
-SpatialNavigation.focus();
+setTimeout(() => {
+  SpatialNavigation.init();
+  SpatialNavigation.add({
+    selector: ".control",
+  });
+  SpatialNavigation.makeFocusable();
+  SpatialNavigation.focus();
 
-var OnKeyUpHandler = function (e) {
-  switch (e.keyCode) {
-    case 13: // Enter
-      alert("You pressed Enter on " + e.target.innerText);
-      break;
-    default:
-      break;
-  }
-};
+  var OnKeyUpHandler = function (e) {
+    switch (e.keyCode) {
+      case 13: // Enter
+        alert("You pressed Enter on " + e.target.innerText);
+        break;
+      default:
+        break;
+    }
+  };
 
-document.addEventListener("keyup", OnKeyUpHandler, false);
+  document.addEventListener("keyup", OnKeyUpHandler, false);
+}, 3000);
